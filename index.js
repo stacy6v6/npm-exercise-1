2a8a7677965a0311b16047c9619a26372035047b
Stacy Pelito <113648447+stacy6v6@users.noreply.github.com>
2022-10-11 09:22:20 +0800

    Added vcard qr generation (#2)
    * added vcard qr generation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let qrcode = require('qrcode-terminal');
 
 console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;'));
+
+console.log(qrcode.generate('BEGIN:VCARD\nN:Stacy;Pelito\nTEL:09177882783\nEMAIL:stacy.pelito@siteminder.com\nEND:VCARD'));
